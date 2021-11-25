a2b9ae02ecf9e6931ab4aa0b6db7bd9f761809ae
hiagrawal <agrawal.hina13@gmail.com>
2021-11-25 13:43:53 +0530

    Assigning Types

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
-function add(num1, num2){
+function add(num1: number, num2: number){
     return num1 + num2;
 }
 
 console.log(add(1,2));
 
-console.log(add('1','2'));
+// console.log(add('1','2'));
 
